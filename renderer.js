20854cac126993c2c5a1d53d2bf73d52c8cf1d35
William Elfversson <wen@furuno.se>
2019-05-20 16:01:57 +0200

    1.0 release

--- renderer.js
+++ renderer.js
@@ -22,12 +22,12 @@ for (i = 0; i < listButtons.length; i++) {
         
             // Create an iframe that points to our PDF.js viewer, and tell PDF.js to open the file that was selected from the file picker.
             const iframe = document.createElement('iframe');
-            iframe.src = path.resolve(__dirname, `./resources/public/pdfjs/web/viewer.html?file=${filePath}`);
+            iframe.src = path.resolve(__dirname, `public/pdfjs/web/viewer.html?file=${filePath}`);
             // Add the iframe to our UI.
             viewerEle.appendChild(iframe)
         } else if (event.target.getAttribute('folderPath')){
            
-            if(shell.openItem(event.target.getAttribute('folderPath'))){
+            if(!shell.openItem(event.target.getAttribute('folderPath'))){
                 dialog.showMessageBox(options, i => console.log(i))
             }else{
                 shell.openItem(event.target.getAttribute('folderPath'))
